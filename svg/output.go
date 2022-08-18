29305dbc4c80692118bdf8acb5fba0c720de1ca3
Mike Cripps <mike.a.cripps@gmail.com>
2022-08-18 02:19:27 +0100

    Render Things

---
+++ svg/output.go
@@ -0,0 +1,381 @@
+package svg
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"strings"
+
+	"github.com/macripps/wad2svg/wad"
+)
+
+type RenderOpts struct {
+	WadName           string
+	MapName           string
+	ImageWidth        int
+	ImageHeight       int
+	RenderArtifacts   bool
+	RenderAmmo        bool
+	RenderKeys        bool
+	RenderMonsters    bool
+	RenderPowerups    bool
+	RenderWeapons     bool
+	RenderMultiplayer bool
+}
+
+func Render(w io.Writer, m *wad.Map, opts *RenderOpts) {
+	minX, minY, maxX, maxY := int16(32767), int16(32767), int16(-32768), int16(-32768)
+	for i := 0; i < len(m.Vertexes); i++ {
+		x := m.Vertexes[i].X
+		y := m.Vertexes[i].Y
+		if x < minX {
+			minX = x
+		}
+		if y < minY {
+			minY = y
+		}
+		if x > maxX {
+			maxX = x
+		}
+		if y > maxY {
+			maxY = y
+		}
+	}
+	fmt.Fprintln(w, "<?xml version=\"1.0\" standalone=\"no\"?>")
+	fmt.Fprintf(w, "<svg width=\"%d\" height=\"%d\" viewBox=\"%d %d %d %d\" xmlns=\"http://www.w3.org/2000/svg\">\n", opts.ImageWidth, opts.ImageHeight, minX, minY, maxX-minX, maxY-minY)
+	fmt.Fprintf(w, "  <title>%s - %s</title>\n", opts.WadName, opts.MapName)
+	fmt.Fprintln(w, "  <g fill-rule=\"evenodd\">")
+
+	sectors := m.Sectors
+	for i, sector := range sectors {
+		fmt.Fprintf(os.Stderr, "Rendering sector #%d/%d\n", i+1, len(sectors))
+		renderSector(w, m, sector, i)
+	}
+	things := m.Things
+	for i, thing := range things {
+		fmt.Fprintf(os.Stderr, "Rendering thing #%d/%d\n", i+1, len(things))
+		renderThing(w, thing, i, opts)
+	}
+	fmt.Fprintln(w, "  </g>")
+	fmt.Fprintln(w, "</svg>")
+}
+
+func renderSector(w io.Writer, m *wad.Map, s wad.Sector, i int) {
+	fmt.Fprintf(w, "    <g %s>\n", s.ToSvgAttributeString())
+	fmt.Fprintf(w, "      <title>Sector %d</title>\n", i)
+	fmt.Fprintf(w, "      <desc>Sector Type: %d</desc>\n", s.SectorType)
+	sectorLineDefs := make([]wad.LineDef, 0)
+	for sd := 0; sd < len(m.SideDefs); sd++ {
+		sidedef := m.SideDefs[sd]
+		if int(sidedef.SectorNumber) == i {
+			for ld := 0; ld < len(m.LineDefs); ld++ {
+				linedef := m.LineDefs[ld]
+				if int(linedef.LeftSideDef) == sd || int(linedef.RightSideDef) == sd {
+					sectorLineDefs = append(sectorLineDefs, linedef)
+				}
+			}
+		}
+	}
+	// Render all sector linedefs with appropriate fill
+	renderAllLineDefs(w, m, sectorLineDefs)
+	// Now render special linedefs again, with colours
+	renderSpecialLineDefs(w, m, sectorLineDefs)
+	fmt.Fprintf(w, "    </g>\n")
+}
+
+func renderAllLineDefs(w io.Writer, m *wad.Map, lds []wad.LineDef) {
+	sectorLineDefs := make([]wad.LineDef, len(lds))
+	copy(sectorLineDefs, lds)
+	var linedefs []wad.LineDef
+	path := strings.Builder{}
+	for len(sectorLineDefs) > 0 {
+		linedefs, sectorLineDefs = selectLineDef(sectorLineDefs, func(ld1, ld2 wad.LineDef) bool {
+			return true
+		})
+		linedef := linedefs[0]
+		start := m.Vertexes[linedef.Start]
+		end := m.Vertexes[linedef.End]
+		path.WriteString(fmt.Sprintf("M %d %d L %d %d ", start.X, start.Y, end.X, end.Y))
+		for i := 1; i < len(linedefs); i++ {
+			v := m.Vertexes[linedefs[i].End]
+			path.WriteString(fmt.Sprintf("%d %d ", v.X, v.Y))
+		}
+	}
+	fmt.Fprintf(w, "    <path d=\"%s\"/>\n", path.String())
+}
+
+func renderSpecialLineDefs(w io.Writer, m *wad.Map, lds []wad.LineDef) {
+	sectorLineDefs := make([]wad.LineDef, 0, len(lds))
+	for _, s := range lds {
+		if s.SpecialType != 0 {
+			sectorLineDefs = append(sectorLineDefs, s)
+		}
+	}
+	for _, linedef := range sectorLineDefs {
+		lineType := linedef.SpecialType
+		if lineType != 0 {
+			stroke := "orange"
+			strokeWidth := 3
+			if linedef.IsDoor() {
+				stroke = "green"
+			} else if linedef.IsTeleporter() {
+				stroke = "red"
+			} else if linedef.IsLift() {
+				stroke = "blue"
+			} else if linedef.IsExit() {
+				stroke = "purple"
+			} else if linedef.IsSecret() {
+				stroke = "aqua"
+			}
+			start := m.Vertexes[linedef.Start]
+			end := m.Vertexes[linedef.End]
+
+			fmt.Fprintf(w, "    <!-- Type %d -->\n", linedef.SpecialType)
+			fmt.Fprintf(w, "    <path d=\"M %d %d L %d %d\" stroke=\"%s\" stroke-width=\"%d\" />", start.X, start.Y, end.X, end.Y, stroke, strokeWidth)
+		}
+	}
+}
+
+func selectLineDef(lineDefs []wad.LineDef, shouldInclude func(wad.LineDef, wad.LineDef) bool) ([]wad.LineDef, []wad.LineDef) {
+	if len(lineDefs) == 1 {
+		return lineDefs, []wad.LineDef{}
+	}
+	nextLineDef := lineDefs[0]
+	lineDefGroup := []wad.LineDef{nextLineDef}
+	lineDefs = lineDefs[1:]
+	for i := 0; i < len(lineDefs); i++ {
+		head := lineDefGroup[len(lineDefGroup)-1]
+		tail := lineDefGroup[0]
+		l := lineDefs[i]
+		if l.Start == head.End &&
+			shouldInclude(l, head) {
+			lineDefGroup = append(lineDefGroup, l)
+			lineDefs[i] = lineDefs[len(lineDefs)-1]
+			lineDefs = lineDefs[:len(lineDefs)-1]
+			i = -1
+		} else if l.End == tail.Start &&
+			shouldInclude(l, tail) {
+			lineDefGroup = append([]wad.LineDef{l}, lineDefGroup...)
+			lineDefs[i] = lineDefs[len(lineDefs)-1]
+			lineDefs = lineDefs[:len(lineDefs)-1]
+			i = -1
+		} else if l.End == head.End && shouldInclude(l, head) {
+			lineDefGroup = append(lineDefGroup, l.Flip())
+			lineDefs[i] = lineDefs[len(lineDefs)-1]
+			lineDefs = lineDefs[:len(lineDefs)-1]
+			i = -1
+		} else if l.Start == tail.Start && shouldInclude(l, tail) {
+			lineDefGroup = append([]wad.LineDef{l.Flip()}, lineDefGroup...)
+			lineDefs[i] = lineDefs[len(lineDefs)-1]
+			lineDefs = lineDefs[:len(lineDefs)-1]
+			i = -1
+		}
+	}
+	return lineDefGroup, lineDefs
+}
+
+func renderThing(w io.Writer, thing wad.Thing, i int, opts *RenderOpts) {
+	if (thing.Flags&16 == 16) && !opts.RenderMultiplayer {
+		return
+	}
+	flags := ""
+	if thing.Flags&1 == 1 {
+		flags = flags + "12"
+	}
+	if thing.Flags&2 == 2 {
+		flags = flags + "3"
+	}
+	if thing.Flags&4 == 4 {
+		flags = flags + "45"
+	}
+	if thing.Flags&8 == 8 {
+		flags = flags + "D"
+	}
+	if thing.Flags&16 == 16 {
+		flags = flags + "M"
+	}
+
+	if opts.RenderAmmo && (thing.ThingType == 17 || thing.ThingType == 2007 || thing.ThingType == 2008 || thing.ThingType == 2010 || thing.ThingType == 2046 || thing.ThingType == 2047 || thing.ThingType == 2048 || thing.ThingType == 2049) {
+		colour := "aqua"
+		var ammoType string
+		switch thing.ThingType {
+		case 17:
+			ammoType = "Energy cell pack"
+		case 2007:
+			ammoType = "Clip"
+		case 2008:
+			ammoType = "4 shotgun shells"
+		case 2010:
+			ammoType = "Rocket"
+		case 2046:
+			ammoType = "Box of rockets"
+		case 2047:
+			ammoType = "Energy cell"
+		case 2048:
+			ammoType = "Box of bullets"
+		case 2049:
+			ammoType = "Box of shotgun shells"
+		}
+		fmt.Fprintf(w, "    <rect x=\"%d\" y=\"%d\" stroke=\"black\" width=\"20\" height=\"20\" fill=\"%s\"><title>%s [%s]</title></rect>\n", thing.XPosition-10, thing.YPosition-10, colour, ammoType, flags)
+	}
+
+	if opts.RenderArtifacts && (thing.ThingType == 83 || thing.ThingType == 2013 || thing.ThingType == 2014 || thing.ThingType == 2015 || thing.ThingType == 2022 || thing.ThingType == 2023 || thing.ThingType == 2024 || thing.ThingType == 2026 || thing.ThingType == 2045) {
+		colour := "green"
+		var artifactType string
+		switch thing.ThingType {
+		case 83:
+			artifactType = "Megasphere"
+		case 2013:
+			artifactType = "Supercharge"
+		case 2014:
+			artifactType = "Health bonus"
+		case 2015:
+			artifactType = "Armor bonus"
+		case 2022:
+			artifactType = "Invulnerability"
+		case 2023:
+			artifactType = "Berserk"
+		case 2024:
+			artifactType = "Partial invisibility"
+		case 2026:
+			artifactType = "Computer area map"
+		case 2045:
+			artifactType = "Light amplification visor"
+		}
+		fmt.Fprintf(w, "    <rect x=\"%d\" y=\"%d\" stroke=\"black\" width=\"20\" height=\"20\" fill=\"%s\"><title>%s [%s]</title></rect>\n", thing.XPosition-10, thing.YPosition-10, colour, artifactType, flags)
+	}
+
+	if opts.RenderKeys && (thing.ThingType == 5 || thing.ThingType == 6 || thing.ThingType == 13 || thing.ThingType == 38 || thing.ThingType == 39 || thing.ThingType == 40) {
+		var colour string
+		var keyType string
+		switch thing.ThingType {
+		case 5:
+			colour = "blue"
+			keyType = "Blue keycard"
+		case 6:
+			colour = "yellow"
+			keyType = "Yellow keycard"
+		case 13:
+			colour = "red"
+			keyType = "Red keycard"
+		case 38:
+			colour = "red"
+			keyType = "Red skull key"
+		case 39:
+			colour = "yellow"
+			keyType = "Yellow skull key"
+		case 40:
+			colour = "blue"
+			keyType = "Blue skull key"
+		}
+		fmt.Fprintf(w, "    <rect x=\"%d\" y=\"%d\" width=\"20\" height=\"20\" fill=\"%s\"><title>%s [%s]</title></rect>\n", thing.XPosition-10, thing.YPosition-10, colour, keyType, flags)
+	}
+
+	if opts.RenderMonsters && (thing.ThingType == 7 || thing.ThingType == 9 || thing.ThingType == 16 || thing.ThingType == 58 || thing.ThingType == 64 || thing.ThingType == 65 || thing.ThingType == 66 || thing.ThingType == 67 || thing.ThingType == 68 || thing.ThingType == 69 || thing.ThingType == 71 || thing.ThingType == 72 || thing.ThingType == 84 || thing.ThingType == 3001 || thing.ThingType == 3002 || thing.ThingType == 3003 || thing.ThingType == 3004 || thing.ThingType == 3005 || thing.ThingType == 3006) {
+		colour := "black"
+		var radius int
+		var monsterType string
+		switch thing.ThingType {
+		case 7:
+			radius = 128
+			monsterType = "Spiderdemon"
+		case 9:
+			radius = 20
+			monsterType = "Shotgun guy"
+		case 16:
+			radius = 40
+			monsterType = "Cyberdemon"
+		case 58:
+			radius = 30
+			monsterType = "Spectre"
+		case 64:
+			radius = 20
+			monsterType = "Arch-vile"
+		case 65:
+			radius = 20
+			monsterType = "Heavy weapon dude"
+		case 66:
+			radius = 20
+			monsterType = "Revenant"
+		case 67:
+			radius = 48
+			monsterType = "Mancubus"
+		case 68:
+			radius = 64
+			monsterType = "Arachnotron"
+		case 69:
+			radius = 24
+			monsterType = "Hell knight"
+		case 71:
+			radius = 31
+			monsterType = "Pain elemental"
+		case 72:
+			radius = 16
+			monsterType = "Commander Keen"
+		case 84:
+			radius = 20
+			monsterType = "Wolfenstein SS"
+		case 3001:
+			radius = 20
+			monsterType = "Imp"
+		case 3002:
+			radius = 30
+			monsterType = "Demon"
+		case 3003:
+			radius = 24
+			monsterType = "Baron of Hell"
+		case 3004:
+			radius = 20
+			monsterType = "Zombieman"
+		case 3005:
+			radius = 31
+			monsterType = "Cacodemon"
+		case 3006:
+			radius = 16
+			monsterType = "Lost soul"
+		}
+		fmt.Fprintf(w, "    <circle cx=\"%d\" cy=\"%d\" r=\"%d\" fill=\"%s\"><title>%s [%s]</title></circle>\n", thing.XPosition-10, thing.YPosition-10, radius, colour, monsterType, flags)
+	}
+
+	if opts.RenderPowerups && (thing.ThingType == 8 || thing.ThingType == 2011 || thing.ThingType == 2012 || thing.ThingType == 2018 || thing.ThingType == 2019 || thing.ThingType == 2025) {
+		colour := "yellow"
+		var powerUpType string
+		switch thing.ThingType {
+		case 8:
+			powerUpType = "Backpack"
+		case 2011:
+			powerUpType = "Stimpack"
+		case 2012:
+			powerUpType = "Medikit"
+		case 2018:
+			powerUpType = "Armor"
+		case 2019:
+			powerUpType = "Megaarmor"
+		case 2025:
+			powerUpType = "Radiation shielding suit"
+		}
+		fmt.Fprintf(w, "    <rect x=\"%d\" y=\"%d\" width=\"20\" height=\"20\" stroke=\"black\" fill=\"%s\"><title>%s [%s]</title></rect>\n", thing.XPosition-10, thing.YPosition-10, colour, powerUpType, flags)
+	}
+
+	if opts.RenderWeapons && (thing.ThingType == 82 || thing.ThingType == 2001 || thing.ThingType == 2002 || thing.ThingType == 2003 || thing.ThingType == 2004 || thing.ThingType == 2005 || thing.ThingType == 2006) {
+		colour := "red"
+		var weaponType string
+		switch thing.ThingType {
+		case 82:
+			weaponType = "Super shotgun"
+		case 2001:
+			weaponType = "Shotgun"
+		case 2002:
+			weaponType = "Chaingun"
+		case 2003:
+			weaponType = "Rocket launcher"
+		case 2004:
+			weaponType = "Plasma gun"
+		case 2005:
+			weaponType = "Chainsaw"
+		case 2006:
+			weaponType = "BFG9000"
+		}
+		fmt.Fprintf(w, "    <rect x=\"%d\" y=\"%d\" width=\"20\" height=\"20\" stroke=\"black\" fill=\"%s\"><title>%s [%s]</title></rect>\n", thing.XPosition-10, thing.YPosition-10, colour, weaponType, flags)
+	}
+}
---
+++ wad/structure.go
@@ -0,0 +1,329 @@
+package wad
+
+import (
+	"encoding/binary"
+	"fmt"
+	"io"
+	"os"
+)
+
+type LineDefFlag uint16
+
+const (
+	BLOCKS_MONSTERS_AND_PLAYERS LineDefFlag = 1 << iota
+	BLOCKS_MONSTERS
+	TWO_SIDED
+	UPPER_TEXTURE_UNPEGGED
+	LOWER_TEXTURE_UNPEGGED
+	SECRET
+	BLOCKS_SOUND
+	NEVER_SHOWN_ON_AUTOMAP
+	ALWAYS_SHOWN_ON_AUTOMAP
+	CAN_ACTIVATE_MORE_THAN_ONCE
+	ACTIVATED_WHEN_USED_BY_PLAYER
+	ACTIVATED_WHEN_CROSSED_BY_MONSTER
+	ACTIVATED_WHEN_BUMPED_BY_PLAYER
+	CAN_BE_ACTIVATED_BY_MONSTERS_OR_PLAYER
+	UNUSED
+	BLOCKS_EVERYTHING
+
+	ACTIVATED_WHEN_HIT_BY_PROJECTILE         = 0x0c00
+	ACTIVATED_WHEN_CROSSED_BY_PROJECTILE     = 0x1400
+	ACTIVATED_WHEN_USED_OR_CROSSED_BY_PLAYER = 0x1800
+)
+
+// TODO(macripps): Support HEXEN/ZDoom LineDef layout
+type LineDef struct {
+	Start        uint16
+	End          uint16
+	Flags        uint16
+	SpecialType  uint16
+	SectorTag    uint16
+	RightSideDef uint16
+	LeftSideDef  uint16
+}
+
+func (l *LineDef) IsDoor() bool {
+	return l.SpecialType == 1 || l.SpecialType == 2 || l.SpecialType == 3 || l.SpecialType == 4 || l.SpecialType == 16
+}
+func (l *LineDef) IsTeleporter() bool {
+	return l.SpecialType == 39 || l.SpecialType == 97 || l.SpecialType == 125 || l.SpecialType == 126 || l.SpecialType == 174 || l.SpecialType == 195 || l.SpecialType == 207 || l.SpecialType == 208 || l.SpecialType == 209 || l.SpecialType == 210 || l.SpecialType == 243 || l.SpecialType == 244 || l.SpecialType == 262 || l.SpecialType == 263 || l.SpecialType == 264 || l.SpecialType == 265 || l.SpecialType == 266 || l.SpecialType == 267 || l.SpecialType == 268 || l.SpecialType == 269
+}
+func (l *LineDef) IsLift() bool {
+	return l.SpecialType == 10 || l.SpecialType == 21 || l.SpecialType == 62 || l.SpecialType == 88 || l.SpecialType == 120 || l.SpecialType == 121 || l.SpecialType == 123
+}
+func (l *LineDef) IsExit() bool {
+	return l.SpecialType == 11 || l.SpecialType == 51 || l.SpecialType == 52 || l.SpecialType == 124 || l.SpecialType == 197 || l.SpecialType == 198
+}
+func (l *LineDef) IsSecret() bool {
+	return l.Flags&uint16(SECRET) == uint16(SECRET)
+}
+func (l LineDef) Flip() LineDef {
+	return LineDef{
+		Start:        l.End,
+		End:          l.Start,
+		Flags:        l.Flags,
+		SpecialType:  l.SpecialType,
+		SectorTag:    l.SectorTag,
+		RightSideDef: l.LeftSideDef,
+		LeftSideDef:  l.RightSideDef,
+	}
+}
+
+type SideDef struct {
+	XOffset           int16
+	YOffset           int16
+	UpperTextureName  string
+	LowerTextureName  string
+	MiddleTextureName string
+	SectorNumber      uint16
+}
+
+type Vertex struct {
+	X int16
+	Y int16
+}
+
+type Sector struct {
+	FloorHeight    int16
+	CeilingHeight  int16
+	FloorTexture   string
+	CeilingTexture string
+	LightLevel     uint16
+	SectorType     uint16
+	TagNumber      uint16
+}
+
+func (s *Sector) isSecret() bool {
+	return s.SectorType == 9
+}
+
+func (s *Sector) isDamage() bool {
+	return s.SectorType == 4 || s.SectorType == 5 || s.SectorType == 7 || s.SectorType == 16
+}
+
+var sectorFill = []string{"white", "white", "white", "white", "red", "red", "unused", "red", "white", "aqua", "green", "purple", "white", "white", "green", "unused", "red", "white"}
+var sectorStroke = []string{"black", "black", "black", "black", "red", "red", "unused", "red", "black", "aqua", "green", "purple", "black", "black", "green", "unused", "red", "black"}
+var sectorOpacity = []string{"1.0", "1.0", "1.0", "1.0", "0.2", "0.1", "unused", "0.05", "1.0", "0.5", "1.0", "1.0", "1.0", "1.0", "1.0", "unused", "0.2", "1.0"}
+
+func (s *Sector) ToSvgAttributeString() string {
+	return fmt.Sprintf("fill=\"%s\" stroke=\"%s\" fill-opacity=\"%s\" stroke-width=\"1\"", sectorFill[s.SectorType], sectorStroke[s.SectorType], sectorOpacity[s.SectorType])
+}
+
+type Thing struct {
+	XPosition int16
+	YPosition int16
+	Angle     uint16
+	ThingType uint16
+	Flags     uint16
+}
+
+type Map struct {
+	LineDefs []LineDef
+	SideDefs []SideDef
+	Vertexes []Vertex
+	Sectors  []Sector
+	Things   []Thing
+}
+
+func (m *Map) parseThings(r io.ReaderAt, size uint32, offset int64) {
+	numThings := size / 10
+	m.Things = make([]Thing, 0, numThings)
+	var t Thing
+	fmt.Fprintf(os.Stderr, "Reading %d things\n", numThings)
+	for numThings > 0 {
+		t, offset = ReadThingFrom(r, offset)
+		m.Things = append(m.Things, t)
+		numThings--
+	}
+}
+
+func (m *Map) parseLineDefs(r io.ReaderAt, size uint32, offset int64) {
+	numLineDefs := size / 14
+	m.LineDefs = make([]LineDef, 0, numLineDefs)
+	var l LineDef
+	fmt.Fprintf(os.Stderr, "Reading %d linedefs\n", numLineDefs)
+	for numLineDefs > 0 {
+		l, offset = ReadLineDefFrom(r, offset)
+		m.LineDefs = append(m.LineDefs, l)
+		numLineDefs--
+	}
+}
+
+func (m *Map) parseSideDefs(r io.ReaderAt, size uint32, offset int64) {
+	numSideDefs := size / 30
+	m.SideDefs = make([]SideDef, 0, numSideDefs)
+	var s SideDef
+	fmt.Fprintf(os.Stderr, "Reading %d sidedefs\n", numSideDefs)
+	for numSideDefs > 0 {
+		s, offset = ReadSideDefFrom(r, offset)
+		m.SideDefs = append(m.SideDefs, s)
+		numSideDefs--
+	}
+}
+
+func (m *Map) parseVertexes(r io.ReaderAt, size uint32, offset int64) {
+	numVertexes := size / 4
+	m.Vertexes = make([]Vertex, 0, numVertexes)
+	fmt.Fprintf(os.Stderr, "Reading %d vertexes\n", numVertexes)
+	var v Vertex
+	for numVertexes > 0 {
+		v, offset = ReadVertexFrom(r, offset)
+		m.Vertexes = append(m.Vertexes, v)
+		numVertexes--
+	}
+}
+
+func (m *Map) parseSectors(r io.ReaderAt, size uint32, offset int64) {
+	numSectors := size / 26
+	m.Sectors = make([]Sector, 0, numSectors)
+	fmt.Fprintf(os.Stderr, "Reading %d sectors\n", numSectors)
+	var s Sector
+	for numSectors > 0 {
+		s, offset = ReadSectorFrom(r, offset)
+		m.Sectors = append(m.Sectors, s)
+		numSectors--
+	}
+}
+
+type LumpType uint32
+
+const (
+	THINGS LumpType = iota
+	LINEDEFS
+	SIDEDEFS
+	VERTEXES
+	SEGS
+	SSECTORS
+	NODES
+	SECTORS
+	REJECT
+	BLOCKMAP
+	BEHAVIOR
+)
+
+type LumpPtr struct {
+	size   uint32
+	offset uint32
+	name   string
+}
+
+func readLump(r io.ReaderAt, offset int64) (*LumpPtr, int64) {
+	var buffer = make([]byte, 8)
+	var lump = &LumpPtr{}
+	r.ReadAt(buffer, offset)
+	lump.offset = binary.LittleEndian.Uint32(buffer[0:4])
+	lump.size = binary.LittleEndian.Uint32(buffer[4:8])
+	r.ReadAt(buffer, offset+8)
+	lump.name = string(buffer)
+	return lump, offset + 16
+}
+
+func (m *Map) ReadFrom(r io.ReaderAt, mapName string) {
+	var buffer = make([]byte, 4)
+	r.ReadAt(buffer, 4)
+	numLumps := binary.LittleEndian.Uint32(buffer)
+	r.ReadAt(buffer, 8)
+	var infoTableOffset = binary.LittleEndian.Uint32(buffer)
+	m.readMapFromInfoTable(r, mapName, int(numLumps), int64(infoTableOffset))
+}
+
+func (m *Map) readMapFromInfoTable(r io.ReaderAt, mapName string, numLumps int, offset int64) {
+	mapNamePadded := (mapName + "\x00\x00\x00\x00\x00\x00\x00\x00")[:8]
+	var lump *LumpPtr
+	found := false
+	for numLumps > 0 {
+		lump, offset = readLump(r, offset)
+		if lump.name == mapNamePadded {
+			fmt.Fprintf(os.Stderr, "Found map %s\n", lump.name)
+			found = true
+		}
+		if found && lump.name == "THINGS\x00\x00" {
+			m.parseThings(r, lump.size, int64(lump.offset))
+		}
+		if found && lump.name == "LINEDEFS" {
+			m.parseLineDefs(r, lump.size, int64(lump.offset))
+		}
+		if found && lump.name == "SIDEDEFS" {
+			m.parseSideDefs(r, lump.size, int64(lump.offset))
+		}
+		if found && lump.name == "VERTEXES" {
+			m.parseVertexes(r, lump.size, int64(lump.offset))
+		}
+		if found && lump.name == "SECTORS\x00" {
+			m.parseSectors(r, lump.size, int64(lump.offset))
+			break
+		}
+		numLumps--
+	}
+}
+
+var linedef = make([]byte, 14)
+
+func ReadLineDefFrom(r io.ReaderAt, offset int64) (LineDef, int64) {
+	r.ReadAt(linedef, offset)
+	l := LineDef{
+		Start:        binary.LittleEndian.Uint16(linedef[0:2]),
+		End:          binary.LittleEndian.Uint16(linedef[2:4]),
+		Flags:        binary.LittleEndian.Uint16(linedef[4:6]),
+		SpecialType:  binary.LittleEndian.Uint16(linedef[6:8]),
+		SectorTag:    binary.LittleEndian.Uint16(linedef[8:10]),
+		RightSideDef: binary.LittleEndian.Uint16(linedef[10:12]),
+		LeftSideDef:  binary.LittleEndian.Uint16(linedef[12:14]),
+	}
+
+	return l, offset + 14
+}
+
+var sidedef = make([]byte, 30)
+
+func ReadSideDefFrom(r io.ReaderAt, offset int64) (SideDef, int64) {
+	r.ReadAt(sidedef, offset)
+	l := SideDef{
+		SectorNumber: binary.LittleEndian.Uint16(sidedef[28:30]),
+	}
+
+	return l, offset + 30
+}
+
+var vertex = make([]byte, 4)
+
+func ReadVertexFrom(r io.ReaderAt, offset int64) (Vertex, int64) {
+	r.ReadAt(vertex, offset)
+	v := Vertex{
+		X: int16(vertex[0]) | int16(vertex[1])<<8,
+		Y: -(int16(vertex[2]) | int16(vertex[3])<<8),
+	}
+
+	return v, offset + 4
+}
+
+var sector = make([]byte, 26)
+
+func ReadSectorFrom(r io.ReaderAt, offset int64) (Sector, int64) {
+	r.ReadAt(sector, offset)
+	s := Sector{
+		FloorHeight:    int16(sector[0]) | int16(sector[1])<<8,
+		CeilingHeight:  int16(sector[2]) | int16(sector[3])<<8,
+		FloorTexture:   string(sector[4:12]),
+		CeilingTexture: string(sector[12:20]),
+		LightLevel:     binary.LittleEndian.Uint16(sector[20:22]),
+		SectorType:     binary.LittleEndian.Uint16(sector[22:24]),
+		TagNumber:      binary.LittleEndian.Uint16(sector[24:26]),
+	}
+	return s, offset + 26
+}
+
+var thing = make([]byte, 10)
+
+func ReadThingFrom(r io.ReaderAt, offset int64) (Thing, int64) {
+	r.ReadAt(thing, offset)
+	t := Thing{
+		XPosition: int16(thing[0]) | int16(thing[1])<<8,
+		YPosition: -(int16(thing[2]) | int16(thing[3])<<8),
+		Angle:     binary.LittleEndian.Uint16(thing[4:6]),
+		ThingType: binary.LittleEndian.Uint16(thing[6:8]),
+		Flags:     binary.LittleEndian.Uint16(thing[8:10]),
+	}
+	return t, offset + 10
+}
